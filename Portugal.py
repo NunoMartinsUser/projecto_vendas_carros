39e375da94a86cae423327a4fb5d9491c94fa1be
Nuno Martins <nunoresendemartins@gmail.com>
2025-10-03 12:28:17 +0100

    primeiro upload

---
+++ Portugal.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+
+st.header('Carros Vendidos em Portugal')
+st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Flag_of_Portugal_%28official%29.svg/2560px-Flag_of_Portugal_%28official%29.svg.png", width=150)
+
+
+df = pd.read_csv('CSVs/carros_europa_combinado.csv')
+
+df_espanha = df[df['País'] == 'Portugal']
+
+st.subheader('🚧 Página em cosntrução 🚧')
+with st.expander('Tabela Completa de Portugal'):
+
+    st.dataframe(df_espanha)
+
+df_filtrado = df_espanha.copy()
+
+filtro = [x for x in df_espanha.columns if x not in ['']]
+
+for x in filtro:
+    opcoes = ['---Selecionar---'] + sorted(df_filtrado[x].unique().tolist())
+    escolha = st.selectbox(f'Selecionar uma opção para {x}', opcoes)
+    if escolha != '---Selecionar---':
+        df_filtrado = df_filtrado[df_filtrado[x] == escolha]
+        st.write(f'Foi selecionado: {escolha}')
