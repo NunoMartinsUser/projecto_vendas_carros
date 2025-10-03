1d5b7d5cd4054211daac6ecd8cefda90c6d7d064
Nuno Martins <nunoresendemartins@gmail.com>
2025-10-03 12:49:58 +0100

    primeiro upload

---
+++ Entrada.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+
+st.header('Carros Vendidos na Europa')
+
+df = pd.read_csv('CSVs/carros_europa_combinado.csv')
+
+
+# filtros = df.columns.difference(["Preço (€)", "Quilometragem (km)"]).tolist()
+filtros = [col for col in df.columns if col not in [
+    'País', "Preço (€)", "Quilometragem (km)"]]
+
+df_filtrado = df.copy()
+
+for coluna in filtros:
+    opcoes = ['-- Selecione um valor --'] + \
+        sorted(df_filtrado[coluna].unique().tolist())
+    escolha = st.selectbox(f"Escolha {coluna}", opcoes)
+
+    if escolha != '-- Selecione um valor --':
+        df_filtrado = df_filtrado[df_filtrado[coluna] == escolha]
+        st.write(f"{coluna} selecionado foi: {escolha}")
+
+if not df_filtrado.equals(df):
+
+    df_grouped = (df_filtrado.groupby(
+        'País').size().reset_index(name='Número de Carros'))
+
+    fig1 = px.bar(df_grouped, x='País', y='Número de Carros', color='País',
+                  title='Número de carros vendidos por país de acordo com os filtos selecionados')
+
+    st.plotly_chart(fig1, use_container_width=True)
+
+    with st.expander('Tabela Completa Filtrada'):
+
+        st.dataframe(df_filtrado)
